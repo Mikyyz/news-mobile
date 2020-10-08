36ef1d862a90f40a33d7b242eea847c998ab50bc
Mikyyz <youzhi.ye1992@gmail.com>
2020-10-09 00:17:33 +0800

    修改tabBar，退出登录时显示未登录

--- toutiao-m/src/views/layout/index.vue
+++ toutiao-m/src/views/layout/index.vue
@@ -5,18 +5,22 @@
             <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
             <van-tabbar-item icon="comment-o" to="/qa">问答</van-tabbar-item>
             <van-tabbar-item icon="video-o" to="/video">视频</van-tabbar-item>
-            <van-tabbar-item icon="user-o" to="/my">我的</van-tabbar-item>
+            <van-tabbar-item icon="user-o" to="/my">{{user ? "我的" : "未登录"}}</van-tabbar-item>
         </van-tabbar>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
     export default {
         name: 'layout',
         data() {
             return {
                 active: 0
             }
+        },
+        computed: {
+           ...mapState(['user'])
         },
     }
 </script>
