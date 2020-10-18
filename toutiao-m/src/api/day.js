07d16bc92e4599ec4ca1f6429f7ee6535fde5158
Mikyyz <youzhi.ye1992@gmail.com>
2020-10-19 00:38:49 +0800

    添加处理相对时间的全局过滤器

---
+++ toutiao-m/src/api/day.js
@@ -0,0 +1,15 @@
+
+import Vue from 'vue';
+import dayjs from 'dayjs';
+//本地化语言配置
+import 'dayjs/locale/zh-cn';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+dayjs.locale('zh-cn') // 全局使用简体中文
+
+//定义一个处理相对时间的全局过滤器
+Vue.filter('relativeTime', value => {
+    return dayjs(value).from(dayjs())
+})
+
