b6ff069502e8342c30206d4c10af46af572fa4af
Mikyyz <youzhi.ye1992@gmail.com>
2020-10-19 00:47:35 +0800

    修改user api模块

--- toutiao-m/src/api/user.js
+++ toutiao-m/src/api/user.js
@@ -40,3 +40,14 @@ export const getCurrentUser = () => {
         // }
     })
 }
+
+/**
+ * 获取用户频道列表
+ */
+
+export const getUserChannerls = () => {
+    return request({
+        method: 'GET',
+        url: `/app/v1_0/user/channels`
+    })
+}
