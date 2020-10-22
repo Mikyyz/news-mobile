9086ecbf5a5938c40606242c0ccb69f1754df9af
Mikyyz <youzhi.ye1992@gmail.com>
2020-10-22 22:58:36 +0800

    完善搜索历史组件

--- toutiao-m/src/views/search/components/search-history.vue
+++ toutiao-m/src/views/search/components/search-history.vue
@@ -9,21 +9,24 @@
                     @click="handleArrowRotate(searchHistories.length)"/>
             </div>
             <div class="right-title" v-if="deleteAction">
-                <span>全部删除</span>
-                <span class="finished">完成</span>
+                <span @click="onDeleteAll">全部删除</span>
+                <span class="finished" @click="deleteAction = false">完成</span>
             </div>
             <van-icon name="delete" v-else @click="deleteAction = true" />
         </van-cell>
         <van-cell v-for="(history, index) in searchHistories" :key="index">
-            <div slot="title" v-if="index <= searchHistoryLength">
+            <div slot="title" v-if="index <= searchHistoryLength" @click="handleSearch(history)">
                 {{ history }}
             </div>
-            <van-icon name="cross" v-if="deleteAction &&  index <= searchHistoryLength"/>
+            <van-icon name="cross" @click="onDeleteOne(index)" v-if="deleteAction &&  index <= searchHistoryLength"/>
         </van-cell>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import store from 'store';
+import { deleteAllHistories } from '@/api/search';
     export default {
         data() {
             return {
@@ -42,10 +45,40 @@
             handleArrowRotate(len) {
                 this.isRotate = !this.isRotate;
                 this.searchHistoryLength = this.searchHistoryLength === 1 ? len : 1;
+            },
+            onDeleteOne(index) {
+                //未登录且删除状态，执行删除操作
+                if(this.deleteAction && !this.user){
+                    this.searchHistories.splice(index, 1);
+                    store.set('search-history', this.searchHistories)
+                } 
+            },
+            //删除全部
+             onDeleteAll() {
+                 this.$dialog.confirm({
+                    message: '是否清空历史记录',
+                    width: '200px'
+                }).then(async () => {
+                    //确认删除
+                    if(this.user) {
+                        //如果是已经登录
+                        await deleteAllHistories();
+                        location.reload();
+                    } else {
+                        //如果没有登录
+                        store.set('search-history', []);
+                        location.reload();
+                    }
+                }).catch(() => {
+                    //捕获错误，什么都不做
+                });
+            },
+            handleSearch(history) {
+                this.$emit('search', history);
             }
         },
         computed: {
-            
+            ...mapState(['user'])
         }, 
               
     }
