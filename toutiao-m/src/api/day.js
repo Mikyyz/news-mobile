872a6e6d5811d2e6683b25ae5a6b163a53aca4fe
Mikyyz <youzhi.ye1992@gmail.com>
2020-10-28 01:06:28 +0800

    增加处理绝对事件的全局过滤器

--- toutiao-m/src/api/day.js
+++ toutiao-m/src/api/day.js
@@ -11,5 +11,10 @@ dayjs.locale('zh-cn') // 全局使用简体中文
 //定义一个处理相对时间的全局过滤器
 Vue.filter('relativeTime', value => {
     return dayjs(value).from(dayjs())
+});
+
+//定义一个处理绝对事件的全局过滤器
+Vue.filter('datetime', (value, formate ='YYYY-MM-DD HH:mm:ss') => {
+    return dayjs(value).format(formate)
 })
 
