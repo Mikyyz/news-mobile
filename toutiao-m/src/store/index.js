94528efcc0c667a652874fb00a3bfad448e0c5d7
Mikyyz <youzhi.ye1992@gmail.com>
2020-11-13 00:09:02 +0800

    添加缓存页面

--- toutiao-m/src/store/index.js
+++ toutiao-m/src/store/index.js
@@ -4,17 +4,32 @@ import store from 'store';
 
 Vue.use(Vuex);
 
-const USER_TOKEN = 'toutiao-user'
+const USER_TOKEN = 'user-token'
 
 export default new Vuex.Store({
   state: {
-    user: store.get(USER_TOKEN) //当前登录用户的状态（token等数据）
+    user: store.get(USER_TOKEN), //当前登录用户的状态（token等数据）
+    pageName: ['layout'] //动态添加缓存页面
   },
   mutations: {
     setUser(state, data) {
       state.user = data;
       //将数据持久化存储在本地
       store.set(USER_TOKEN, state.user);
+    },
+    //添加缓存页面
+    addCachePages(state, pageName) {
+      const isPageNameExist = state.pageName.includes(pageName)
+      if (!isPageNameExist) {
+        state.pageName.push(pageName)
+      }
+    },
+    //移除缓存页面
+    removeCachePages(state, pageName) {
+      const index = state.pageName.indexOf(pageName);
+      if (index !== -1) {
+        state.pageName.splice(index, 1);
+      }
     }
   },
   actions: {},
