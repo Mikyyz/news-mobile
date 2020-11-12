d97c83d53618fc4fe39d90da2973f41c3009c716
Mikyyz <youzhi.ye1992@gmail.com>
2020-11-13 00:10:41 +0800

    添加pageName，用于缓存页面

--- toutiao-m/src/App.vue
+++ toutiao-m/src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
+    <keep-alive :include="pageName">
       <router-view/>
+    </keep-alive>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
+export default {
+      computed: {
+        ...mapState(['pageName'])
+      },
+  }
 </script>
 
 <style lang="less">
